d9256ea937592af9ad220d92486939f536abcd4f
Rohan <harichandanrohan24@gmail.com>
2024-10-11 18:14:28 +0530

    first commit v0

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request
+import subprocess
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        code = request.form['code']
+        output = subprocess.check_output(['python', '-c', code])
+        return render_template('index.html', code=code, output=output.decode('utf-8'))
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
